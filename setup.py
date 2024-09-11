71f4e68f281b5c257f40ca9f47f21db44babaab8
Byron Price <byron_price@hms.harvard.edu>
2024-09-11 19:02:13 -0400

    setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools 
+
+setuptools.setup(
+    name='6cam_pipeline',
+    version='0.0.0',
+    author='Byron Price',
+    author_email='byron.h.price@gmail.com',
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.7',
+    install_requires=[],
+    url='https://github.com/byronprice/6cam_pipeline',
+)
